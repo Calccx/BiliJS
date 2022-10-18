81181fde15904e01dd45f1778f3c67337ed55e16
Calccx <allcalc@outlook.com>
2022-10-18 21:27:41 +0800

    完成框架

--- 哔哩哔哩倍速.js
+++ 哔哩哔哩倍速.js
@@ -1,4 +1,19 @@
-var changeSpeed = document.querySelector('.bpx-player-ctrl-playbackrate-menu-item');
-changeSpeed.setAttribute('data-value',0.5);// 这里设置需要的倍数
-changeSpeed.innerHTML=('0.5x');
-changeSpeed.click();
+
+//修改速度函数
+function changeSpeed(){
+    var speedvalue=document.getElementById(InputerId).value
+    var changeSpeed = document.querySelector('.bpx-player-ctrl-playbackrate-menu-item');
+    changeSpeed.setAttribute('data-value',speedvalue);// 这里设置需要的倍数
+    changeSpeed.innerHTML=('0.5x');
+    changeSpeed.click();
+}
+//输入框
+var Inputer=document.createElement("input");
+Inputer.type="text";Inputer.name="InputerName";Inputer.id="InputerId";Inputer.width="50"
+//提交栏
+var upload=document.createElement("button")
+upload.onclick="changeSpeed()"
+//标题下栏
+var titleblank=document.getElementsByClassName("video-data")[0]
+titleblank.appendChild(Inputer)
+titleblank.appendChild(upload)
