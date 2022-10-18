4ee50a0e5e67fdf213875fdc8f0557ed137c55bf
Calccx <allcalc@outlook.com>
2022-10-18 20:09:47 +0800

    得到速度修改的方法，实现黑暗模式的方法

---
+++ 哔哩哔哩倍速.js
@@ -0,0 +1,4 @@
+var changeSpeed = document.querySelector('.bpx-player-ctrl-playbackrate-menu-item');
+changeSpeed.setAttribute('data-value',0.5);// 这里设置需要的倍数
+changeSpeed.innerHTML=('0.5x');
+changeSpeed.click();
---
+++ 测试代码.js
@@ -0,0 +1,5 @@
+var inputer=document.createElement("p");
+var textf=document.createTextNode=("文本");
+inputer.appendChild(textf);
+var ele=document.getElementById("idv");
+ele.appendChild(inputer);
