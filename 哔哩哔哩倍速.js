0de99e03cd33e631aa1689792c0989db54e2cdfc
Calccx <allcalc@outlook.com>
2022-10-18 23:13:23 +0800

    完成倍速js

--- 哔哩哔哩倍速.js
+++ 哔哩哔哩倍速.js
@@ -1,18 +1,17 @@
-
 //修改速度函数
-function changeSpeed(){
-    var speedvalue=document.getElementById(InputerId).value
-    var changeSpeed = document.querySelector('.bpx-player-ctrl-playbackrate-menu-item');
-    changeSpeed.setAttribute('data-value',speedvalue);// 这里设置需要的倍数
-    changeSpeed.innerHTML=('0.5x');
-    changeSpeed.click();
-}
+function changeSpeeder(){
+var speedvalue=document.getElementById("InputerId").value
+alert(speedvalue)
+var changeSpeed = document.querySelector('.bpx-player-ctrl-playbackrate-menu-item');
+changeSpeed.setAttribute('data-value',speedvalue);
+changeSpeed.innerHTML=(speedvalue+'x');
+changeSpeed.click();}
 //输入框
 var Inputer=document.createElement("input");
-Inputer.type="text";Inputer.name="InputerName";Inputer.id="InputerId";Inputer.width="50"
+Inputer.type="text";Inputer.name="InputerName";Inputer.id="InputerId";Inputer.size="1"
 //提交栏
-var upload=document.createElement("button")
-upload.onclick="changeSpeed()"
+var upload=document.createElement("input")
+upload.onclick=changeSpeeder;upload.type="submit";upload.value="修改速度"
 //标题下栏
 var titleblank=document.getElementsByClassName("video-data")[0]
 titleblank.appendChild(Inputer)
